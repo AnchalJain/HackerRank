88c07d9f00715bafd648d7da3f031f8b9ca76f20
AnchalJain <anchalj109@gmail.com>
2019-01-21 19:57:46 +0530

    intent properly

--- sock_merchant.py
+++ sock_merchant.py
@@ -8,31 +8,31 @@ import sys
 
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-        sock={}
-            for i in ar:
-                        if not i in sock:
-                                        sock[i] = 1
-                                                else:
-                                                                sock[i] += 1
-                                                                    print(sock)
-                                                                        value=sock.values()
-                                                                            count=0
-                                                                                for i in value:
-                                                                                            count=count+i//2
-                                                                                                return count
+    sock={}
+    for i in ar:
+        if not i in sock:
+            sock[i] = 1
+        else:
+            sock[i] += 1
+    print(sock)
+    value=sock.values()
+    count=0
+    for i in value:
+        count=count+i//2
+    return count
 
 
 
-                                                                                            if __name__ == '__main__':
-                                                                                                    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-                                                                                                        n = int(input())
+    n = int(input())
 
-                                                                                                            ar = list(map(int, input().rstrip().split()))
+    ar = list(map(int, input().rstrip().split()))
 
-                                                                                                                result = sockMerchant(n, ar)
+    result = sockMerchant(n, ar)
 
-                                                                                                                    fptr.write(str(result) + '\n')
+    fptr.write(str(result) + '\n')
 
-                                                                                                                        fptr.close()
+    fptr.close()
 
